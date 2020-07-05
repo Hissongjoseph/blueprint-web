0c75df19f46aabe4262caf1ac98ee76eca4edf4b
Joseph Hissong <hissong.joseph@gmail.com>
2020-07-05 11:12:05 -0400

    board selection in navbar

--- src/app/nav/navbar/navbar/navbar.component.ts
+++ src/app/nav/navbar/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
+import { FirebaseService } from 'src/app/services/firebase/firebase.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -9,14 +10,37 @@ import { auth } from 'firebase/app';
 })
 export class NavbarComponent implements OnInit {
   @Input()
-  auth: AngularFireAuth
+  auth: AngularFireAuth;
+  currentUserUID: string;
 
-  constructor() { }
+  projectNames: Array<any>;
+  projectDetails: Array<any>;
+  selectedProject!: string;
+
+  constructor(
+    private firebaseService: FirebaseService
+  ) { }
 
   ngOnInit(): void {
+    this.projectNames = [];
+    this.selectedProject = '';
+
+    this.currentUserUID = this.auth.auth.currentUser.email;
+
+    this.firebaseService.getBoardNames(this.currentUserUID)
+      .then(result => {
+        this.projectNames = result;
+      }
+    );
   }
 
   logout() {
     this.auth.auth.signOut();
   }
+
+  onChange(event: any) {
+    this.selectedProject = event.target.value;
+    localStorage.setItem('board', this.selectedProject);
+  }
+
 }
