79abeac436f6d9003eed54d03bbad6ab9a8c2154
Joseph Hissong <hissong.joseph@gmail.com>
2020-08-08 17:00:07 -0400

    listing boards you are part of.

--- src/app/boards/manage-boards/manage-boards/manage-boards.component.ts
+++ src/app/boards/manage-boards/manage-boards/manage-boards.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase/firebase.service';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-manage-boards',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageBoardsComponent implements OnInit {
 
-  constructor() { }
+  boardNames: Array<string>;
+  currentUserUID: string;
+
+  constructor(
+    private firebaseService: FirebaseService,
+    private auth: AngularFireAuth
+    ) { }
 
   ngOnInit(): void {
+    this.boardNames = [];
+
+    this.currentUserUID = this.auth.auth.currentUser.email;
+
+    this.firebaseService.getBoardNames(this.currentUserUID).then(
+      results => {
+        this.boardNames = results['companies'];
+      }
+    );
   }
 
 }
